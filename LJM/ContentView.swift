53abfecb9e68316ab1fa1edb82e22438fee71729
Laura Benetti <laura.benetti21@outlook.it>
2020-11-25 10:49:26 +0100

    Initial Commit

---
+++ LJM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LJM
+//
+//  Created by Laura Benetti on 25/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LJM/LJMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LJMApp.swift
+//  LJM
+//
+//  Created by Laura Benetti on 25/11/20.
+//
+
+import SwiftUI
+
+@main
+struct LJMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
