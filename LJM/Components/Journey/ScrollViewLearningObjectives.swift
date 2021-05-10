ee8888c85edd1e8ee57b7e7f67f034920ded4f4e
Tony <toni.tresgots@gmail.com>
2021-05-10 10:30:25 +0200

    Fix big bug

--- LJM/Components/Journey/ScrollViewLearningObjectives.swift
+++ LJM/Components/Journey/ScrollViewLearningObjectives.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ScrollViewLearningObjectives: View {
     @Environment(\.colorScheme) var colorScheme
     
-//    @EnvironmentObject var learningPathsStore: LearningPathStore
-//    @EnvironmentObject var studentLearningObjectivesStore: StudentLearningObjectivesStore
-//    @EnvironmentObject var mapLearningObjectivesStore: MapLearningObjectivesStore
-//    @EnvironmentObject var challengeStore: ChallengesStore
+    //    @EnvironmentObject var learningPathsStore: LearningPathStore
+    //    @EnvironmentObject var studentLearningObjectivesStore: StudentLearningObjectivesStore
+    //    @EnvironmentObject var mapLearningObjectivesStore: MapLearningObjectivesStore
+    //    @EnvironmentObject var challengeStore: ChallengesStore
     
     @ObservedObject var totalLOs : TotalNumberLearningObjectives
     
@@ -32,17 +32,17 @@ struct ScrollViewLearningObjectives: View {
                 .filter { $0.isCore ?? false }
                 .sorted { $0.learningGoal?.lowercased() ?? "No Title" < $1.learningGoal?.lowercased() ?? "NoTitle"}
         case "Elective":
-
             return sortLearningObjectives(learningObj: storage.studentLearningObjectives)
                 .filter { (!($0.isCore ?? false) ) }
                 .sorted { $0.learningGoal?.lowercased() ?? "No Title" < $1.learningGoal?.lowercased() ?? "NoTitle"}
         case "Evaluated":
-
             return sortLearningObjectives(learningObj: storage.studentLearningObjectives)
                 .filter { $0.assessments?.first?.value ?? 0 > 0 }
                 .sorted { $0.learningGoal?.lowercased() ?? "No Title" < $1.learningGoal?.lowercased() ?? "NoTitle"}
         case "All":
+            print(storage.learningObjectives.compactMap { $0.assessments })
             return sortLearningObjectives(learningObj: storage.studentLearningObjectives)
+                .filter { ($0.assessments?.count ?? 0) > 0 }
                 .sorted { $0.learningGoal?.lowercased() ?? "No Title" < $1.learningGoal?.lowercased() ?? "NoTitle"}
         default:
             return filteredLearningObjectivesMap
@@ -62,7 +62,7 @@ struct ScrollViewLearningObjectives: View {
             } else {
                 return filteredChallenges
             }
-
+            
         }
     }
     
@@ -83,36 +83,30 @@ struct ScrollViewLearningObjectives: View {
     var selectedStrands: [String]
     
     var body: some View {
-        
-        ScrollView(showsIndicators: true) {
-            
-            LazyVStack {
-                ForEach(filteredLearningObjectives) { item in
-                    if textFromSearchBar.isEmpty || (item.learningGoal!.lowercased().contains(textFromSearchBar.lowercased())) || ((item.description!.lowercased().contains(textFromSearchBar.lowercased()))) {
-                        if item.strand != nil {
-                            if self.selectedStrands.contains(item.strand!.strand) || self.selectedStrands.count == 0 {
-                                LearningObjectiveJourneyCell(rating: item.assessments?.first?.value ?? 0, isRatingView: isAddable ? true : false, isAddable: isAddable, learningPathSelected: self.$learningPathSelected, learningObj: item)
-                                    .background(colorScheme == .dark ? Color(red: 30/255, green: 30/255, blue: 30/255) : .white)
-                                    .contextMenu {
-                                        if !isAddable {
-                                            Button {
-                                                if item.id != nil {
-                                                    Webservices.deleteLearningObjectiveFromStudentJourney(id: item.id!) { (deletedLearningObj, err) in
-                                                      //  self.studentLearningObjectivesStore.removeItem(item)
-                                                        storage.studentLearningObjectives.remove(object: item)
-                                                    }
-                                                }
-                                            } label: {
-                                                Text("Delete")
+        List(filteredLearningObjectives) { item in
+            if textFromSearchBar.isEmpty || (item.learningGoal!.lowercased().contains(textFromSearchBar.lowercased())) || ((item.description!.lowercased().contains(textFromSearchBar.lowercased()))) {
+                if item.strand != nil {
+                    if self.selectedStrands.contains(item.strand!.strand) || self.selectedStrands.count == 0 {
+                        LearningObjectiveJourneyCell(rating: item.assessments?.first?.value ?? 0, isRatingView: isAddable ? true : false, isAddable: isAddable, learningPathSelected: self.$learningPathSelected, learningObj: item)
+                            .background(colorScheme == .dark ? Color(red: 30/255, green: 30/255, blue: 30/255) : .white)
+                            .contextMenu {
+                                if !isAddable {
+                                    Button {
+                                        if item.id != nil {
+                                            Webservices.deleteLearningObjectiveFromStudentJourney(id: item.id!) { (deletedLearningObj, err) in
+                                                //  self.studentLearningObjectivesStore.removeItem(item)
+                                                storage.studentLearningObjectives.remove(object: item)
                                             }
                                         }
+                                    } label: {
+                                        Text("Delete")
                                     }
+                                }
                             }
-                        }
                     }
                 }
             }
-        }
+        }.removeBackground()
         .onChange(of: self.filteredLearningObjectives) { result in
             self.totalLOs.total = result.count
         }
@@ -142,9 +136,9 @@ struct ScrollViewLearningObjectives: View {
     
     func displayFullMapLearningObjectives(learningPaths: [LearningPath], selectedFilter: String?) -> [LearningObjective] {
         var arrayFullMapLearningObjectives : [LearningObjective] = [LearningObjective]()
-    
+        
         for learningPath in learningPaths {
-
+            
             if selectedFilter != nil || selectedFilter != "" {
                 arrayFullMapLearningObjectives.append(contentsOf: learningPath.learningObjectives!)
             } else {
@@ -156,35 +150,35 @@ struct ScrollViewLearningObjectives: View {
     
     func sortLearningObjectives(learningObj: [LearningObjective]) -> [LearningObjective] {
         
-    //    var arrayOfLearningObjectives : [LearningObjective] = [LearningObjective]()
+        //    var arrayOfLearningObjectives : [LearningObjective] = [LearningObjective]()
         
         return learningObj
         
-//        if learningPaths != nil && learningPaths.count > 0 {
-//            for learningPath in learningPaths {
-//                if selectedPath != "" {
-//                    if learningPath.title?.lowercased() == selectedPath.lowercased() {
-//                        for learningObjective in self.studentLearningObjectivesStore.learningObjectives {
-//                            if learningObjective.learningPaths != nil {
-//                                for lp in learningObjective.learningPaths! {
-//                                    if lp._id == learningPath.id {
-//                                        print("OIJUHYHIJ \(learningObjective)")
-//                                        arrayOfLearningObjectives.append(learningObjective)
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                } else {
-//                    print("IJOUHYBINJOK")
-//                    arrayOfLearningObjectives.append(contentsOf: self.studentLearningObjectivesStore.learningObjectives)
-//                    break
-//                }
-//            }
-//            return arrayOfLearningObjectives
-//        } else {
-//            return arrayOfLearningObjectives
-//        }
+        //        if learningPaths != nil && learningPaths.count > 0 {
+        //            for learningPath in learningPaths {
+        //                if selectedPath != "" {
+        //                    if learningPath.title?.lowercased() == selectedPath.lowercased() {
+        //                        for learningObjective in self.studentLearningObjectivesStore.learningObjectives {
+        //                            if learningObjective.learningPaths != nil {
+        //                                for lp in learningObjective.learningPaths! {
+        //                                    if lp._id == learningPath.id {
+        //                                        print("OIJUHYHIJ \(learningObjective)")
+        //                                        arrayOfLearningObjectives.append(learningObjective)
+        //                                    }
+        //                                }
+        //                            }
+        //                        }
+        //                    }
+        //                } else {
+        //                    print("IJOUHYBINJOK")
+        //                    arrayOfLearningObjectives.append(contentsOf: self.studentLearningObjectivesStore.learningObjectives)
+        //                    break
+        //                }
+        //            }
+        //            return arrayOfLearningObjectives
+        //        } else {
+        //            return arrayOfLearningObjectives
+        //        }
     }
     
     func sortLearningObjectivesByChallenge(challenges: [LearningPath], selectedChallenge: String) -> [LearningObjective] {
@@ -193,14 +187,14 @@ struct ScrollViewLearningObjectives: View {
         
         if challenges != nil && challenges.count > 0 {
             for challenge in challenges {
-
+                
                 if selectedChallenge != "" {
                     if challenge.title!.lowercased().replacingOccurrences(of: "challenge ", with: "") == selectedChallenge.lowercased() {
                         arrayOfLearningObjectives.append(contentsOf: challenge.learningObjectives ?? [LearningObjective]())
                     }
                 } else {
                     for learningObjective in storage.challenges {
-
+                        
                         arrayOfLearningObjectives.append(contentsOf:learningObjective.learningObjectives ?? [LearningObjective]())
                     }
                     break
@@ -218,7 +212,7 @@ struct ScrollViewLearningObjectives: View {
         
         if learningPaths != nil && learningPaths.count > 0 {
             for learningPath in learningPaths {
-
+                
                 if selectedPath != "" {
                     if learningPath.title!.lowercased() == selectedPath.lowercased() {
                         arrayOfLearningObjectives.append(contentsOf: learningPath.learningObjectives ?? [LearningObjective]())
@@ -239,7 +233,7 @@ struct ScrollViewLearningObjectives: View {
         var value = 0
         if assessments != nil {
             for assessment in assessments! {
-
+                
                 if assessment.learningObjectiveId == learningObjectiveId {
                     value = assessment.value ?? 0
                 }
--- LJM/Extensions/Extensions.swift
+++ LJM/Extensions/Extensions.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import AppKit
+import Introspect
 
 // Remove ugly focus ring on textfields borders.
 
@@ -51,4 +52,17 @@ extension Bool: Comparable {
             return false
         }
     }
+}
+
+// Remove List background color bugged
+extension List {
+  /// List on macOS uses an opaque background with no option for
+  /// removing/changing it. listRowBackground() doesn't work either.
+  /// This workaround works because List is backed by NSTableView.
+  func removeBackground() -> some View {
+    return introspectTableView { tableView in
+      tableView.backgroundColor = .clear
+      tableView.enclosingScrollView!.drawsBackground = false
+    }
+  }
 }
