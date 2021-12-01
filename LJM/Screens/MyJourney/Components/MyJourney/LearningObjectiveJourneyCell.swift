5c098f01000cc807ac5bd8913e0c09a1eb523ccc
TonyTresgots <toni.tresgots@gmail.com>
2021-12-01 11:42:41 +0100

    clean code

--- LJM/Screens/MyJourney/Components/MyJourney/LearningObjectiveJourneyCell.swift
+++ LJM/Screens/MyJourney/Components/MyJourney/LearningObjectiveJourneyCell.swift
@@ -370,16 +370,3 @@ struct LearningObjectiveJourneyCell: View {
         }
     }
 }
-
-struct AnimatingCellHeight: AnimatableModifier {
-    var height: CGFloat = 0
-    
-    var animatableData: CGFloat {
-        get { height }
-        set { height = newValue }
-    }
-    
-    func body(content: Content) -> some View {
-        content.frame(height: height)
-    }
-}
